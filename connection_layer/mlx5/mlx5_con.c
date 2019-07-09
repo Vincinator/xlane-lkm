b7ee952869a7c3085f294cbcfcc4f54bffb8891c
Vincent <Vincent.Riesop@gmail.com>
2019-07-09 11:10:36 +0200

    fixed encoding issues with source

--- connection_layer/mlx5/mlx5_con.c
+++ connection_layer/mlx5/mlx5_con.c
@@ -17,11 +17,11 @@ struct sassy_mlx5_con_info info;
 
 int sassy_mlx5_con_register_channel(int ix, int cqn){
 
-	info.ix = ix;
-	info.cqn = cqn;
-	sassy_dbg("Channel %d registered with cqn=%d", ix, cqn);
+    info.ix = ix;
+    info.cqn = cqn;
+    sassy_dbg("Channel %d registered with cqn=%d", ix, cqn);
 
-	return 0;
+    return 0;
 }
 EXPORT_SYMBOL(sassy_mlx5_con_register_channel);
 
@@ -29,16 +29,16 @@ EXPORT_SYMBOL(sassy_mlx5_con_register_channel);
 static int __init sassy_mlx5_con_init(void)
 {
 
-	sassy_dbg("init\n");
+    sassy_dbg("init\n");
 
-	return 0;
+    return 0;
 }
 
 
 static void __exit sassy_mlx5_con_exit(void) 
 {
 
-	sassy_dbg("exit\n");
+    sassy_dbg("exit\n");
 
 }
 
