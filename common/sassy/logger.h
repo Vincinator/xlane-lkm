86d85f502e378f1c8e119f2e70b16da13b826a1a
Vincent <Vincent.Riesop@gmail.com>
2019-07-09 17:35:56 +0200

    auto commit message

--- common/sassy/logger.h
+++ common/sassy/logger.h
@@ -9,18 +9,18 @@
 /* 
  * Prefixes NIC device ID and SASSY Context (e.g. [SASSY][NIC4][CONSENSUS])
  */
-#define sassy_dbg(format, arg...)						\
+#define sassy_dbg(format, ...)						\
 ({														\
 	if (1)												\
-		printk(KERN_DEBUG LOG_PREFIX format, ##arg);	\
+		printk(KERN_DEBUG LOG_PREFIX format, ## __VA_ARGS__);	\
 })
 
 /* 
  * Prefixes NIC device ID and SASSY Context (e.g. [SASSY][NIC4][CONSENSUS])
  */
-#define sassy_error(format, arg...)						\
+#define sassy_error(format, ...)						\
 ({														\
 	if (1)												\
-		printk(KERN_ERR LOG_PREFIX format, ##arg);		\
+		printk(KERN_ERR LOG_PREFIX format, ## __VA_ARGS__);		\
 })
 #endif /* _SASSY_LOGGER_H_ */
