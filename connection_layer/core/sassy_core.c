af486c94079325635ec776006afab2894d7bc77d
Vincent <Vincent.Riesop@gmail.com>
2019-07-15 10:09:57 +0200

    auto commit message

--- connection_layer/core/sassy_core.c
+++ connection_layer/core/sassy_core.c
@@ -25,175 +25,177 @@ static int device_counter = 0;
 
 
 void sassy_reset_remote_host_counter(int sassy_id){
-	int i;
-	struct sassy_rx_table *rxt;
-	struct sassy_device *sdev;
-	struct sassy_pm_target_info *pmtarget;
+    int i;
+    struct sassy_rx_table *rxt;
+    struct sassy_device *sdev;
+    struct sassy_pm_target_info *pmtarget;
 
-	if(sassy_validate_sassy_device(sassy_id))
-		return;
+    if(sassy_validate_sassy_device(sassy_id))
+        return;
 
-	rxt = score->rx_tables[sassy_id];
-	sdev = score->sdevices[sassy_id];
+    rxt = score->rx_tables[sassy_id];
+    sdev = score->sdevices[sassy_id];
 
-	/* Free Memory of all  */
-	for(i = 0; i < MAX_REMOTE_SOURCES; i++){
+    /* Free Memory of all  */
+    for(i = 0; i < MAX_REMOTE_SOURCES; i++){
 
-		pmtarget = &sdev->pminfo.pm_targets[i];
+        pmtarget = &sdev->pminfo.pm_targets[i];
 
-		kfree(rxt->rhost_buffers[i]);
-		kfree(pmtarget->hb_pkt_params->hb_payload);
-		kfree(pmtarget->hb_pkt_params->dst_mac);
-		kfree(pmtarget->hb_pkt_params);
-	}
-	sdev->pminfo.num_of_targets = 0;
-	sassy_dbg(" set num_of_targets to 0\n");
+        kfree(rxt->rhost_buffers[i]);
+        kfree(pmtarget->hb_pkt_params->hb_payload);
+        kfree(pmtarget->hb_pkt_params->dst_mac);
+        kfree(pmtarget->hb_pkt_params);
+    }
+    sdev->pminfo.num_of_targets = 0;
+    sassy_dbg(" set num_of_targets to 0\n");
 }
 EXPORT_SYMBOL(sassy_reset_remote_host_counter);
 
 int sassy_generate_next_id(void) 
 {
-	if(device_counter >= SASSY_MLX5_DEVICES_LIMIT) {
-		sassy_error("Reached Limit of maximum connected mlx5 devices.\n");
-		sassy_error("Limit=%d, device_counter=%d\n", SASSY_MLX5_DEVICES_LIMIT, device_counter);
-		return -1;
-	}
+    if(device_counter >= SASSY_MLX5_DEVICES_LIMIT) {
+        sassy_error("Reached Limit of maximum connected mlx5 devices.\n");
+        sassy_error("Limit=%d, device_counter=%d\n", SASSY_MLX5_DEVICES_LIMIT, device_counter);
+        return -1;
+    }
 
-	return device_counter++;
+    return device_counter++;
 }
 
 int sassy_core_write_packet(int sassy_id, int remote_id) {
-	sassy_dbg("Not implemented: %s\n", __FUNCTION__);
-	return 0;
+    sassy_dbg("Not implemented: %s\n", __FUNCTION__);
+    return 0;
 }
 
 
 /* Called by Connection Layer Glue (e.g. mlx5_con.c) */
 int sassy_core_register_nic(int ifindex) 
 {
-	char name_buf[MAX_SYNCBEAT_PROC_NAME];
-	int sassy_id;
+    char name_buf[MAX_SYNCBEAT_PROC_NAME];
+    int sassy_id;
 
-	sassy_dbg("register nic at sassy core\n");
+    sassy_dbg("register nic at sassy core\n");
 
-	sassy_id = sassy_generate_next_id();
+    sassy_id = sassy_generate_next_id();
 
-	if(sassy_id < 0)
-		return -1;
+    if(sassy_id < 0)
+        return -1;
 
 
-	score->rx_tables[sassy_id] = kmalloc(sizeof(struct sassy_rx_table), GFP_KERNEL);
-	score->rx_tables[sassy_id]->rhost_buffers = kmalloc_array(MAX_REMOTE_SOURCES, sizeof(struct sassy_rx_buffer*), GFP_KERNEL);
-	
-	score->sdevices[sassy_id] = kmalloc(sizeof(struct sassy_device), GFP_KERNEL);
-	score->sdevices[sassy_id]->ifindex = ifindex;
-	score->sdevices[sassy_id]->sassy_id = sassy_id;
+    score->rx_tables[sassy_id] = kmalloc(sizeof(struct sassy_rx_table), GFP_KERNEL);
+    score->rx_tables[sassy_id]->rhost_buffers = kmalloc_array(MAX_REMOTE_SOURCES, sizeof(struct sassy_rx_buffer*), GFP_KERNEL);
+    
+    score->sdevices[sassy_id] = kmalloc(sizeof(struct sassy_device), GFP_KERNEL);
+    score->sdevices[sassy_id]->ifindex = ifindex;
+    score->sdevices[sassy_id]->sassy_id = sassy_id;
     score->sdevices[sassy_id]->ndev = sassy_get_netdevice(ifindex);
-	score->sdevices[sassy_id]->pminfo.num_of_targets = 0;
+    score->sdevices[sassy_id]->pminfo.num_of_targets = 0;
 
 
-	snprintf(name_buf,  sizeof name_buf, "sassy/%d", ifindex);
-	proc_mkdir(name_buf, NULL);
+    snprintf(name_buf,  sizeof name_buf, "sassy/%d", ifindex);
+    proc_mkdir(name_buf, NULL);
 
-	/* Initialize Control Interfaces for NIC */
-	init_sassy_pm_ctrl_interfaces(score->sdevices[sassy_id]);
+    /* Initialize Control Interfaces for NIC */
+    init_sassy_pm_ctrl_interfaces(score->sdevices[sassy_id]);
 
 
-	/* Initialize Component States*/
+    /* Initialize Component States*/
     pm_state_transition_to(&score->sdevices[sassy_id]->pminfo, SASSY_PM_UNINIT);
 
-	return sassy_id;
+    return sassy_id;
 }
 EXPORT_SYMBOL(sassy_core_register_nic);
 
 
 int sassy_core_remove_nic(int sassy_id)
 {
-	int i;
-	char name_buf[MAX_SYNCBEAT_PROC_NAME];
+    int i;
+    char name_buf[MAX_SYNCBEAT_PROC_NAME];
 
-	if(sassy_validate_sassy_device(sassy_id)){
-		return -1;
-	}
+    if(sassy_validate_sassy_device(sassy_id)){
+        return -1;
+    }
 
-	/* Remove Ctrl Interfaces for NIC */
-	clean_sassy_pm_ctrl_interfaces(score->sdevices[sassy_id]);
+    /* Remove Ctrl Interfaces for NIC */
+    clean_sassy_pm_ctrl_interfaces(score->sdevices[sassy_id]);
 
-	snprintf(name_buf,  sizeof name_buf, "sassy/%d", score->sdevices[sassy_id]->ifindex);
-	proc_mkdir(name_buf, NULL);
+    snprintf(name_buf,  sizeof name_buf, "sassy/%d", score->sdevices[sassy_id]->ifindex);
+    proc_mkdir(name_buf, NULL);
 
-	/* Free Memory used for this NIC */
+    /* Free Memory used for this NIC */
 
-	for(i = 0; i < MAX_PROCESSES_PER_HOST; i++){
-		kfree(score->rx_tables[sassy_id]->rhost_buffers[i]);
-	}
-	kfree(score->rx_tables[sassy_id]);
-	kfree(score->sdevices[sassy_id]);
+    for(i = 0; i < MAX_PROCESSES_PER_HOST; i++){
+        kfree(score->rx_tables[sassy_id]->rhost_buffers[i]);
+    }
+    kfree(score->rx_tables[sassy_id]);
+    kfree(score->sdevices[sassy_id]);
 
 }
 
 int sassy_validate_sassy_device(int sassy_id) {
-	if(!score){
-		sassy_error("score is NULL!\n");
-		return -1;
-	}
-	if(sassy_id < 0 || sassy_id > MAX_NIC_DEVICES){
-		sassy_error("invalid sassy_id! %d\n", sassy_id);
-		return -1;
-	}
+    
+    if(!score){
+        sassy_error("score is NULL!\n");
+        return -1;
+    }
 
-	if(!score->sdevices || !score->sdevices[sassy_id]){
-		sassy_error("sdevices is invalid!\n");
-		return -1;
-	}
+    if(sassy_id < 0 || sassy_id > MAX_NIC_DEVICES){
+        sassy_error("invalid sassy_id! %d\n", sassy_id);
+        return -1;
+    }
 
-	if(!score->rx_tables || !score->rx_tables[sassy_id]){
-		sassy_error("rx_tables is invalid!\n");
-		return -1;
-	}
-	return 0;
+    if(!score->sdevices || !score->sdevices[sassy_id]){
+        sassy_error("sdevices is invalid!\n");
+        return -1;
+    }
+
+    if(!score->rx_tables || !score->rx_tables[sassy_id]){
+        sassy_error("rx_tables is invalid!\n");
+        return -1;
+    }
+    return 0;
 }
 EXPORT_SYMBOL(sassy_validate_sassy_device);
 
 int sassy_core_register_remote_host(int sassy_id, uint32_t ip, char *mac)
 {
-	struct sassy_rx_table *rxt;
-	struct sassy_device *sdev;
-	struct sassy_pm_target_info *pmtarget;
-	int ifindex;
+    struct sassy_rx_table *rxt;
+    struct sassy_device *sdev;
+    struct sassy_pm_target_info *pmtarget;
+    int ifindex;
 
-	if(!mac){
-		sassy_error("input mac is NULL!\n");
-		return -1;
-	}
+    if(!mac){
+        sassy_error("input mac is NULL!\n");
+        return -1;
+    }
 
-	if(sassy_validate_sassy_device(sassy_id)){
-		return -1;
-	}
+    if(sassy_validate_sassy_device(sassy_id)){
+        return -1;
+    }
 
 
-	if(sdev->pminfo.num_of_targets >= MAX_REMOTE_SOURCES) {
-		sassy_error("Reached Limit of remote hosts. \n");
-		sassy_error("Limit is=%d, num_of_targets= %d \n", MAX_REMOTE_SOURCES, sdev->pminfo.num_of_targets);
-		return -1;
-	}
+    if(sdev->pminfo.num_of_targets >= MAX_REMOTE_SOURCES) {
+        sassy_error("Reached Limit of remote hosts. \n");
+        sassy_error("Limit is=%d, num_of_targets= %d \n", MAX_REMOTE_SOURCES, sdev->pminfo.num_of_targets);
+        return -1;
+    }
 
-	rxt = score->rx_tables[sassy_id];
-	sdev = score->sdevices[sassy_id];
+    rxt = score->rx_tables[sassy_id];
+    sdev = score->sdevices[sassy_id];
 
-	ifindex = sdev->ifindex;
-	pmtarget = &sdev->pminfo.pm_targets[sdev->pminfo.num_of_targets];
+    ifindex = sdev->ifindex;
+    pmtarget = &sdev->pminfo.pm_targets[sdev->pminfo.num_of_targets];
 
-	rxt->rhost_buffers[sdev->pminfo.num_of_targets] = kmalloc(sizeof(struct sassy_rx_buffer), GFP_KERNEL);
+    rxt->rhost_buffers[sdev->pminfo.num_of_targets] = kmalloc(sizeof(struct sassy_rx_buffer), GFP_KERNEL);
 
-	pmtarget->hb_pkt_params = kzalloc(sizeof(struct sassy_hb_packet_params), GFP_KERNEL);
-	pmtarget->hb_pkt_params->hb_payload = kzalloc(sizeof(struct sassy_heartbeat_payload), GFP_KERNEL);
-	pmtarget->hb_pkt_params->dst_mac = kmalloc(sizeof(unsigned char) * 6, GFP_KERNEL);
+    pmtarget->hb_pkt_params = kzalloc(sizeof(struct sassy_hb_packet_params), GFP_KERNEL);
+    pmtarget->hb_pkt_params->hb_payload = kzalloc(sizeof(struct sassy_heartbeat_payload), GFP_KERNEL);
+    pmtarget->hb_pkt_params->dst_mac = kmalloc(sizeof(unsigned char) * 6, GFP_KERNEL);
 
-	pmtarget->hb_pkt_params->dst_ip = ip;
-	memcpy(pmtarget->hb_pkt_params->dst_mac, mac, sizeof(unsigned char) * 6);
+    pmtarget->hb_pkt_params->dst_ip = ip;
+    memcpy(pmtarget->hb_pkt_params->dst_mac, mac, sizeof(unsigned char) * 6);
 
-	return sdev->pminfo.num_of_targets++;
+    return sdev->pminfo.num_of_targets++;
 
 }
 EXPORT_SYMBOL(sassy_core_register_remote_host);
@@ -201,53 +203,53 @@ EXPORT_SYMBOL(sassy_core_register_remote_host);
 
 static int __init sassy_connection_core_init(void)
 {
-	sassy_dbg("init\n");
+    sassy_dbg("init\n");
 
-	score = kmalloc(sizeof(struct sassy_core), GFP_KERNEL);
+    score = kmalloc(sizeof(struct sassy_core), GFP_KERNEL);
 
-	if(!score) {
-		sassy_error("allocation of sassy core failed\n");
-		return -1;
-	}
+    if(!score) {
+        sassy_error("allocation of sassy core failed\n");
+        return -1;
+    }
 
-	score->rx_tables = kmalloc_array(MAX_NIC_DEVICES, sizeof(struct sassy_rx_table *), GFP_KERNEL);
+    score->rx_tables = kmalloc_array(MAX_NIC_DEVICES, sizeof(struct sassy_rx_table *), GFP_KERNEL);
 
-	if(!score->rx_tables) {
-		sassy_error("allocation of score->rx_tables failed\n");
-		return -1;
-	}
+    if(!score->rx_tables) {
+        sassy_error("allocation of score->rx_tables failed\n");
+        return -1;
+    }
 
-	score->sdevices = kmalloc_array(MAX_NIC_DEVICES, sizeof(struct sassy_device *), GFP_KERNEL);
+    score->sdevices = kmalloc_array(MAX_NIC_DEVICES, sizeof(struct sassy_device *), GFP_KERNEL);
 
-	if(!score->rx_tables) {
-		sassy_error("allocation of score->sdevices failed\n");
-		return -1;
-	}
+    if(!score->rx_tables) {
+        sassy_error("allocation of score->sdevices failed\n");
+        return -1;
+    }
 
 
-	proc_mkdir("sassy", NULL);
+    proc_mkdir("sassy", NULL);
 
-	sassy_dbg("init done\n");
+    sassy_dbg("init done\n");
 
-	return 0;
+    return 0;
 }
 
 static void __exit sassy_connection_core_exit(void) 
 {
-	int i;
+    int i;
 
-	sassy_dbg("cleanup\n");
+    sassy_dbg("cleanup\n");
 
-	for(i = 0; i < device_counter; i++){
-		sassy_core_remove_nic(i);
-	} 
-
-
-	kfree(score);
+    for(i = 0; i < device_counter; i++){
+        sassy_core_remove_nic(i);
+    } 
 
 
+    kfree(score);
 
-	sassy_dbg("cleanup done\n");
+
+
+    sassy_dbg("cleanup done\n");
 
 }
 
