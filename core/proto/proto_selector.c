f63f1e216ca31122698e4fe26b341078e8bd2b90
Vincent <Vincent.Riesop@gmail.com>
2019-07-18 18:07:09 +0200

    auto commit message

--- core/proto/proto_selector.c
+++ core/proto/proto_selector.c
@@ -64,7 +64,7 @@ static int proto_selector_show(struct seq_file *m, void *v)
 		return -1;
 	}
 
-	copy_to_user(name_buf, sproto->name, sizeof(sproto->name));
+	copy_to_user(name_buf, sdev->proto->name, sizeof(sdev->proto->name));
 
 	seq_printf(m, "sdev uses protocol %s with id %d\n", name_buf, sdev->proto->protocol_id);
 
