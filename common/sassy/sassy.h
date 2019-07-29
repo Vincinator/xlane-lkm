ae9d17b836b9d7cc2496887065cbaf6a7ec324e4
Vincent <Vincent.Riesop@gmail.com>
2019-07-29 10:37:43 +0200

    auto commit message

--- common/sassy/sassy.h
+++ common/sassy/sassy.h
@@ -168,7 +168,7 @@ struct sassy_protocol_ctrl_ops {
 
 	int (*stop)(struct sassy_device*);
 
-	int (*us_update)(struct sassy_device*);
+	int (*us_update)(struct sassy_device*, void *payload);
 
 	/* free memory of app and remove user space interfaces */
 	int (*clean) (struct sassy_device*);
