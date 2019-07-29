5cc579570af02ad084a217ee891a9449b4478d85
Vincent <Vincent.Riesop@gmail.com>
2019-07-29 11:34:24 +0200

    auto commit message

--- core/pm/sassy_pm.c
+++ core/pm/sassy_pm.c
@@ -158,8 +158,8 @@ int sassy_heart(void *data)
         for(i = 0; i < spminfo->num_of_targets; i++) {
 
             // Always update payload to avoid jitter!
-            hb_active_ix    =  spminfo->pm_targets[i].pkt_data.hb_active_ix;
-            pkt_payload      = &spminfo->pm_targets[i].pkt_data.pkt_payload[hb_active_ix];
+            hb_active_ix    = spminfo->pm_targets[i].pkt_data.hb_active_ix;
+            pkt_payload     = spminfo->pm_targets[i].pkt_data.pkt_payload[hb_active_ix];
 
             sassy_update_skb_payload(spminfo->pm_targets[i].skb, pkt_payload);
 
