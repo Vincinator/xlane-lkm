d22e1b885ed9e5736539e963390708c9c98545f6
Vincent <Vincent.Riesop@gmail.com>
2019-07-30 11:31:32 +0200

    auto commit message

--- common/sassy/logger.h
+++ common/sassy/logger.h
@@ -1,6 +1,7 @@
 #ifndef _SASSY_LOGGER_H_
 #define _SASSY_LOGGER_H_
 
+
 #ifndef LOG_PREFIX
 #define LOG_PREFIX "[SASSY][UNKNOWN MODULE]"
 #endif
@@ -8,16 +9,16 @@
 /* 
  * Prefixes NIC device ID and SASSY Context (e.g. [SASSY][NIC4][CONSENSUS])
  */
-#define sassy_dbg(format, arg...)                                              \
-	({                                                                     \
-		if (1)                                                         \
-			printk(KERN_DEBUG LOG_PREFIX format, ##arg);           \
-	})
+#define sassy_dbg(format, arg...)						\
+({														\
+	if (1)												\
+		printk(KERN_DEBUG LOG_PREFIX format, ##arg);	\
+})
 
-#define sassy_error(format, arg...)                                            \
-	({                                                                     \
-		if (1)                                                         \
-			sassy_error(LOG_PREFIX format, ##arg);                 \
-	})
+#define sassy_error(format, arg...)						\
+({														\
+	if (1)												\
+		printk(KERN_ERR LOG_PREFIX format, ##arg);	\
+})
 
-#endif /* _SASSY_LOGGER_H_ */
+#endif  /* _SASSY_LOGGER_H_ */
