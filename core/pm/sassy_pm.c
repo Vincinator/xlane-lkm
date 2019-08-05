1237b9eb188d97c0b273c1434f8a359f920381ba
Vincent <Vincent.Riesop@gmail.com>
2019-08-05 09:46:02 +0200

    auto commit message

--- core/pm/sassy_pm.c
+++ core/pm/sassy_pm.c
@@ -166,24 +166,24 @@ int sassy_heart(void *data)
 
 		for (i = 0; i < spminfo->num_of_targets; i++) {
 			// Always update payload to avoid jitter!
-			// hb_active_ix =
-			// 	spminfo->pm_targets[i].pkt_data.hb_active_ix;
-			// pkt_payload =
-			// 	spminfo->pm_targets[i].pkt_data.pkt_payload[hb_active_ix];
+			hb_active_ix =
+			     spminfo->pm_targets[i].pkt_data.hb_active_ix;
+			pkt_payload =
+			     spminfo->pm_targets[i].pkt_data.pkt_payload[hb_active_ix];
 
-			// // Direct Updates - No double buffer
-			// if (sdev->proto->ctrl_ops.us_update != NULL)
-			// 	sdev->proto->ctrl_ops.us_update(sdev,
-			// 					pkt_payload);
+			//Direct Updates - No double buffer
+            if (sdev->proto->ctrl_ops.us_update != NULL)
+			 sdev->proto->ctrl_ops.us_update(sdev,
+			 					pkt_payload);
 
-			// sassy_update_skb_payload(spminfo->pm_targets[i].skb,
-			// 			 pkt_payload);
+			sassy_update_skb_payload(spminfo->pm_targets[i].skb,
+						 pkt_payload);
 
-			// if (sdev->verbose)
-			// 	print_hex_dump(KERN_DEBUG,
-			// 		       "Payload: ", DUMP_PREFIX_NONE,
-			// 		       16, 1, pkt_payload,
-			// 		       SASSY_PAYLOAD_BYTES, 0);
+			if (sdev->verbose)
+			     print_hex_dump(KERN_DEBUG,
+			 	       "Payload: ", DUMP_PREFIX_NONE,
+			 		       16, 1, pkt_payload,
+			 		       SASSY_PAYLOAD_BYTES, 0);
 
 			sassy_send_hb(sdev->ndev, spminfo->pm_targets[i].skb);
 
