c67f5552004e51ac01ef8b0e15483d138b73b557
Vincent <Vincent.Riesop@gmail.com>
2019-08-07 15:52:12 +0200

    auto commit message

--- logic_layer/consensus/sassy_consensus.c
+++ logic_layer/consensus/sassy_consensus.c
@@ -11,6 +11,10 @@ MODULE_VERSION("0.01");
 #undef LOG_PREFIX
 #define LOG_PREFIX "[SASSY][CONSENSUS]"
 
+struct sassy_protocol consensus_protocol;
+static struct sassy_fd_priv consensus_priv;
+
+
 static const struct sassy_protocol_ctrl_ops consensus_ops = {
 	.init = consensus_init,
 	.start = consensus_start,
@@ -26,14 +30,20 @@ static const struct sassy_protocol_ctrl_ops consensus_ops = {
 
 static int __init sassy_consensus_init(void)
 {
-	sassy_dbg("init\n");
+	sassy_dbg("init consensus protocol\n");
 
+	fd_protocol.proto_type = SASSY_PROTO_CONSENSUS;
+	fd_protocol.ctrl_ops = consensus_ops;
+	fd_protocol.name = "consensus";
+	fd_protocol.priv = (void *)&consensus_priv;
+
+	sassy_register_protocol(&consensus_protocol);
 	return 0;
 }
 
 static void __exit sassy_consensus_exit(void)
 {
-	sassy_dbg("exit\n");
+	sassy_dbg("exit consensus protocol\n");
 }
 
 module_init(sassy_consensus_init);
