f40f37c068cf27496ecfca53ae974dc645bb18ee
Vincent <Vincent.Riesop@gmail.com>
2019-08-09 14:02:04 +0200

    auto commit message

--- logic_layer/consensus/include/follower.h
+++ logic_layer/consensus/include/follower.h
@@ -4,7 +4,7 @@
 
 
 int follower_process_pkt(struct sassy_device *sdev, void* pkt);
-int reset_timeout(void);
+void reset_timeout(void);
 int stop_follower(void);
 int start_follower(void);
 ktime_t get_rnd_timeout(void);
