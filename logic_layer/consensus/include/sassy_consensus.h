5dfcdf1bf5755031c84a554061c5612373a23a41
Vincent <Vincent.Riesop@gmail.com>
2019-08-21 18:23:28 +0200

    auto commit message

--- logic_layer/consensus/include/sassy_consensus.h
+++ logic_layer/consensus/include/sassy_consensus.h
@@ -99,3 +99,4 @@ ktime_t get_rnd_timeout(void);
 void set_le_opcode(unsigned char *pkt, enum le_opcode opcode, u32 p1, u32 p2);
 void accept_leader(struct sassy_device *sdev, int remote_lid, u32 term);
 int setup_le_broadcast_msg(struct sassy_device *sdev, enum le_opcode opcode);
+int setup_le_msg(struct pminfo *spminfo, enum le_opcode opcode, u32 target_id, u32 term);
