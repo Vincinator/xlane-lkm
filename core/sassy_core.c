d25a7e8e58bb885b29f5839024685d4e0c293f01
Vincent <Vincent.Riesop@gmail.com>
2019-09-06 08:45:25 +0200

    auto commit message

--- core/sassy_core.c
+++ core/sassy_core.c
@@ -254,7 +254,7 @@ static int sassy_core_remove_nic(int sassy_id)
 
 	remove_proto_selector(score->sdevices[sassy_id]);
 
-	remove_logger_ifaces(score->sdevices[sassy_id]->le_logger);
+	remove_logger_ifaces(&score->sdevices[sassy_id]->le_logger);
 
 	snprintf(name_buf, sizeof(name_buf), "sassy/%d",
 		 score->sdevices[sassy_id]->ifindex);
