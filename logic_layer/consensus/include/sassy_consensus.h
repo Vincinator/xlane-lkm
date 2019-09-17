2422861feee214db72f93a0ddbcc04bffbadc280
Vincent <Vincent.Riesop@gmail.com>
2019-09-17 15:07:31 +0200

    auto commit message

--- logic_layer/consensus/include/sassy_consensus.h
+++ logic_layer/consensus/include/sassy_consensus.h
@@ -95,8 +95,8 @@ ktime_t get_rnd_timeout_plus(int plus);
 ktime_t get_rnd_timeout_candidate_plus(int plus);
 
 void set_le_opcode(unsigned char *pkt, enum le_opcode opcode, u32 p1, u32 p2);
-void accept_leader(struct sassy_device *sdev, int remote_lid, int cluster_id, u32 term);
-int setup_le_broadcast_msg(struct sassy_device *sdev, enum le_opcode opcode);
+void accept_leader(struct consensus_priv *priv, int remote_lid, int cluster_id, u32 term);
+int setup_le_broadcast_msg(struct consensus_priv *priv, enum le_opcode opcode);
 int setup_le_msg(struct pminfo *spminfo, enum le_opcode opcode, u32 target_id, u32 term);
 void log_le_rx(int verbose, enum node_state nstate, uint64_t ts, int term, enum le_opcode opcode, int rcluster_id, int rterm);
 void log_le_vote(int verbose, enum node_state nstate, uint64_t ts, int term, int cur_votes);
