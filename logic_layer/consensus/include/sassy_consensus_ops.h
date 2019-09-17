77046a8029f003767e6fa003b2e86d46f5fbdc46
Vincent <Vincent.Riesop@gmail.com>
2019-09-17 14:40:34 +0200

    auto commit message

--- logic_layer/consensus/include/sassy_consensus_ops.h
+++ logic_layer/consensus/include/sassy_consensus_ops.h
@@ -2,15 +2,15 @@
 
 #include <sassy/sassy.h>
 
-int consensus_init(struct sassy_device *);
-int consensus_start(struct sassy_device *);
-int consensus_stop(struct sassy_device *);
-int consensus_clean(struct sassy_device *);
-int consensus_info(struct sassy_device *);
+int consensus_init(struct proto_instance *ins);
+int consensus_start(struct proto_instance *ins);
+int consensus_stop(struct proto_instance *ins);
+int consensus_clean(struct proto_instance *ins);
+int consensus_info(struct proto_instance *ins);
 
-int consensus_post_payload(struct sassy_device *sdev, unsigned char *remote_mac,
+int consensus_post_payload(struct proto_instance *ins, unsigned char *remote_mac,
 		    void *payload);
-int consensus_post_ts(struct sassy_device *sdev, unsigned char *remote_mac,
+int consensus_post_ts(struct proto_instance *ins, unsigned char *remote_mac,
 	       uint64_t ts);
-int consensus_us_update(struct sassy_device *sdev, void *payload);
+int consensus_us_update(struct proto_instance *ins, void *payload);
 int consensus_init_payload(void *payload);
