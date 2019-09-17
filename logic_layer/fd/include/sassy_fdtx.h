3196cf9974b6388c756d311ca73a495fc257bf4a
Vincent <Vincent.Riesop@gmail.com>
2019-09-17 18:06:38 +0200

    auto commit message

--- logic_layer/fd/include/sassy_fdtx.h
+++ logic_layer/fd/include/sassy_fdtx.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <sassy/sassy.h>
 
+#include "sassy_fd.h"
+
 int sassy_bypass_init_class(struct sassy_device *sdev);
 void sassy_clean_class(struct sassy_device *sdev);
 int sassy_setup_chardev(struct sassy_device *sdev, struct sassy_fd_priv *priv);
