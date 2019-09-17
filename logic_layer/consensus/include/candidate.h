9f50431f8e3c162141cdbdca804fdf3de5699151
Vincent <Vincent.Riesop@gmail.com>
2019-09-17 16:20:29 +0200

    auto commit message

--- logic_layer/consensus/include/candidate.h
+++ logic_layer/consensus/include/candidate.h
@@ -6,8 +6,8 @@
 
 int candidate_process_pkt(struct proto_instance *ins, int remote_lid, int rcluster_id, unsigned char *pkt);
 void reset_ctimeout(struct proto_instance *ins);
-int stop_candidate(struct consensus_priv *priv);
-int start_candidate(struct consensus_priv *priv);
+int stop_candidate(struct proto_instance *priv);
+int start_candidate(struct proto_instance *priv);
 void init_ctimeout(struct proto_instance *ins);
 int setup_nomination(struct proto_instance *ins);
 
