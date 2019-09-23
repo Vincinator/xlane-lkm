0f5c46e841a89848e026596438d68c9010d13fec
Vincent <Vincent.Riesop@gmail.com>
2019-09-23 16:49:25 +0200

    auto commit message

--- common/sassy/consensus.h
+++ common/sassy/consensus.h
@@ -35,6 +35,47 @@ enum node_state {
 	LEADER = 2,
 };
 
+
+struct sm_command{
+	u32 sm_logvar_id;
+	u32 sm_logvar_value;
+};
+
+
+struct sm_log_entry {
+
+	/* Term in which this command was appended to the log
+	 */
+	u32 term;
+
+	/* The command for the state machine.
+	 * Contains the required information to change the state machine.
+	 *
+	 * A ordered set of commands applied to the state machine will 
+	 * transition the state machine to a common state (shared across the cluster).
+	 */ 
+	struct sm_command cmd;
+};
+
+
+struct sm_log {
+
+	/* Index of the last valid entry in the entries array
+	 */
+	u32 last_idx;
+
+	/* Index of the last commited entry in the entries array 
+	 */
+	u32 commit_idx;
+
+	/* Maximum index of the entries array
+	 */
+	u32 max_entries;
+
+	struct sm_log_entry **entries;
+
+};
+
 struct consensus_priv {
 
 	struct sassy_device *sdev;
@@ -79,45 +120,6 @@ struct consensus_priv {
 	int votes;
 };
 
-struct sm_command{
-	u32 sm_logvar_id;
-	u32 sm_logvar_value;
-};
-
-
-struct sm_log_entry {
-
-	/* Term in which this command was appended to the log
-	 */
-	u32 term;
-
-	/* The command for the state machine.
-	 * Contains the required information to change the state machine.
-	 *
-	 * A ordered set of commands applied to the state machine will 
-	 * transition the state machine to a common state (shared across the cluster).
-	 */ 
-	struct sm_command cmd;
-};
-
-
-struct sm_log {
-
-	/* Index of the last valid entry in the entries array
-	 */
-	u32 last_idx;
-
-	/* Index of the last commited entry in the entries array 
-	 */
-	u32 commit_idx;
-
-	/* Maximum index of the entries array
-	 */
-	u32 max_entries;
-
-	struct sm_log_entry **entries;
-
-};
 
 int commit_upto_index(struct sm_log *log, u32 index);
 int append_command(struct sm_log *log, struct sm_command *cmd);
