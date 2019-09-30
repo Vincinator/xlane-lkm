afdf78ef562c54c98c9324bcb7be9734229b80f9
Vincent <Vincent.Riesop@gmail.com>
2019-09-30 10:51:10 +0200

    auto commit message

--- logic_layer/consensus/candidate.c
+++ logic_layer/consensus/candidate.c
@@ -201,9 +201,9 @@ int candidate_process_pkt(struct proto_instance *ins, int remote_lid, int rclust
 
 	u8 opcode = GET_CON_PROTO_OPCODE_VAL(pkt);
 	s32 param1 = GET_CON_PROTO_PARAM1_VAL(pkt);
-	// s32 param2 = GET_CON_PROTO_PARAM2_VAL(pkt);
-	// s32 param3 = GET_CON_PROTO_PARAM3_VAL(pkt);
-	// s32 param4 = GET_CON_PROTO_PARAM4_VAL(pkt);
+	s32 param2 = GET_CON_PROTO_PARAM2_VAL(pkt);
+	s32 param3 = GET_CON_PROTO_PARAM3_VAL(pkt);
+	s32 param4 = GET_CON_PROTO_PARAM4_VAL(pkt);
 
 #if 1
 	log_le_rx(sdev->verbose, priv->nstate, rdtsc(), priv->term, opcode, rcluster_id, param1);
