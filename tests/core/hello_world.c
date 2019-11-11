9a847e9b3ad0651d4a2f8aba02c960cae24aa20a
Vincent <Vincent.Riesop@gmail.com>
2019-11-11 18:14:45 +0100

    test

--- tests/core/hello_world.c
+++ tests/core/hello_world.c
@@ -7,10 +7,19 @@
 static void hello_world(void **state) {
     (void) state; /* unused */
 }
+/* A test case that does nothing and succeeds. */
+static void hello_world2(void **state) {
+    (void) state; /* unused */
+    assert_int_equal(1, 0);
+}
 
-int main(void) {
-    const struct CMUnitTest tests[] = {
-        cmocka_unit_test(hello_world),
-    };
-    return cmocka_run_group_tests(tests, NULL, NULL);
+const struct CMUnitTest hello_world_tests[] = {
+    cmocka_unit_test(hello_world),
+    cmocka_unit_test(hello_world2),
+};
+
+
+int main(void)
+{
+    return cmocka_run_group_tests(hello_world_tests, NULL, NULL);
 }
