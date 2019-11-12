bef6e65b8b8ee2835d71cb2e07a880be68afc61f
Vincent <Vincent.Riesop@gmail.com>
2019-11-12 16:19:34 +0100

    added append_rpc_check demo test

---
+++ tests/raft/kunit_follower.c
@@ -0,0 +1,36 @@
+#include <kunit/test.h>
+#include <asguard/consensus.h>
+
+
+static void asguard_test_check_append_rpc(struct kunit *test)
+{
+    //pkt_size, prev_log_term, prev_log_idx, max_entries_per_pkt
+
+    /* If pkt size is -1, do not append */
+    KUNIT_EXPECT_EQ(test, 1, _check_append_rpc(-1, 1, 1, 10));
+
+    /* If log is full, do not append */
+    KUNIT_EXPECT_EQ(test, 1, _check_append_rpc(10, 1, MAX_CONSENSUS_LOG, 10));
+
+    /* pkt size within limit */
+    KUNIT_EXPECT_EQ(test, 0,
+    _check_append_rpc(AE_ENTRY_SIZE * 100 + ASGUARD_PROTO_CON_AE_BASE_SZ, 1, 5, 100));
+
+    /* pkt size exceeds limit */
+    KUNIT_EXPECT_EQ(test, 1,
+    _check_append_rpc(AE_ENTRY_SIZE * 100 + ASGUARD_PROTO_CON_AE_BASE_SZ + 1, 1, 5, 100));
+
+}
+
+
+
+static struct kunit_case asguard_raft_follower_test_cases[] = {
+    KUNIT_CASE(asguard_test_check_append_rpc),
+    {}
+};
+
+static struct kunit_suite asguard_raft_follower_test_suite = {
+    .name = "asguard_raft_follower",
+    .test_cases = asguard_raft_follower_test_cases,
+};
+kunit_test_suite(asguard_raft_follower_test_suite);
