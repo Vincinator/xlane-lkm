dcf76b114dba4fed0279a3a36676d955d515c673
Vincent <Vincent.Riesop@gmail.com>
2019-11-20 14:09:41 +0100

    check if mem is available

--- core/asguard_core.c
+++ core/asguard_core.c
@@ -316,11 +316,17 @@ int asguard_core_remove_device_stats(int asguard_id)
 	struct asguard_stats *stats = score->sdevices[asguard_id]->stats;
 
 	for (i = 0; i < stats->timestamp_amount; i++) {
-		kfree(stats->timestamp_logs[i]->timestamp_items);
-		kfree(stats->timestamp_logs[i]->name);
-		kfree(stats->timestamp_logs[i]->proc_dir);
+
+		if(tats->timestamp_logs[i]->timestamp_items)
+			kfree(stats->timestamp_logs[i]->timestamp_items);
+
+		if(stats->timestamp_logs[i]->name)
+			kfree(stats->timestamp_logs[i]->name);
+
 		kfree(stats->timestamp_logs[i]);
 	}
+	asguard_dbg("Freed memory for stats of asguard device %d", asguard_id);
+
 	kfree(stats);
 
 	return 0;
