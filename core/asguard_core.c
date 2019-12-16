8e708b78ff0a51b46970c4e4f4362383d0fcc4af
Vincent <Vincent.Riesop@gmail.com>
2019-12-16 16:07:33 +0100

    dbg test

--- core/asguard_core.c
+++ core/asguard_core.c
@@ -286,8 +286,8 @@ void asguard_post_payload(int asguard_id, unsigned char *remote_mac, void *paylo
 
 	received_proto_instances = GET_PROTO_AMOUNT_VAL(payload);
 
-	_handle_sub_payloads(sdev, remote_mac, GET_PROTO_START_SUBS_PTR(payload),
-		received_proto_instances, cqe_bcnt);
+//	_handle_sub_payloads(sdev, remote_mac, GET_PROTO_START_SUBS_PTR(payload),
+//		received_proto_instances, cqe_bcnt);
 
 
 	// asguard_process_pkt_payload(sdev,remote_mac, payload, cqe_bcnt, remote_lid);
