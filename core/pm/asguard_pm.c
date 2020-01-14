d6eca225c58935fc1147ebb3bb325b5917e7de11
Vincent <Vincent.Riesop@gmail.com>
2020-01-14 13:32:52 +0100

    only schedule from app logic

--- core/pm/asguard_pm.c
+++ core/pm/asguard_pm.c
@@ -345,8 +345,8 @@ static inline int _emit_pkts(struct asguard_device *sdev,
 
 	if(sdev->consensus_priv->nstate != LEADER)
 		update_leader(sdev, spminfo);
-	else
-		prepare_log_replication(sdev); // check if logs must be replicated (schedules work)
+	// else
+	// 	prepare_log_replication(priv->sdev); // check if logs must be replicated (schedules work)
 
 	return 0;
 }
