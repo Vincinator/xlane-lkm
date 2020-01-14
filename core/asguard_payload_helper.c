7ba1ba3a2325fd9a18e883aefc0bafcfed6f48d4
Vincent <Vincent.Riesop@gmail.com>
2020-01-14 14:10:51 +0100

    continue to rep log if there is more work

--- core/asguard_payload_helper.c
+++ core/asguard_payload_helper.c
@@ -379,8 +379,11 @@ void prepare_log_replication_handler(struct work_struct *w)
 
 	more = _do_prepare_log_replication(aw->sdev);
 
-	// ignore more
-	aw->sdev->block_leader_wq = 0;
+	if(more) {
+		_schedule_log_rep(sdev);
+	} else {
+		aw->sdev->block_leader_wq = 0;
+	}
 
 	kfree(aw);
 
