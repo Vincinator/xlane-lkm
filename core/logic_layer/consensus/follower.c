d8c092c774d5bf18f308b778d00ea6298c5bb6b0
Vincent <Vincent.Riesop@gmail.com>
2020-01-20 12:11:15 +0100

    fix

--- core/logic_layer/consensus/follower.c
+++ core/logic_layer/consensus/follower.c
@@ -397,14 +397,17 @@ void _handle_append_rpc(struct proto_instance *ins, struct consensus_priv *priv,
 				priv->sm_log.next_retrans_req_idx = -2;
 			}
 		}
-		reply_append(ins, &priv->sdev->pminfo, remote_lid, rcluster_id, priv->term, 2, priv->sm_log.next_retrans_req_idx);
-
+		if(priv->sm_log.next_retrans_req_idx != -1)
+			goto reply_retransmission;
 
 	}
-	else
-		reply_append(ins, &priv->sdev->pminfo, remote_lid, rcluster_id, priv->term, 1, priv->sm_log.stable_idx);
 
-
+// default: reply success
+	reply_append(ins, &priv->sdev->pminfo, remote_lid, rcluster_id, priv->term, 1, priv->sm_log.stable_idx);
+	priv->sdev->fire = 1;
+	return;
+reply_retransmission;
+	reply_append(ins, &priv->sdev->pminfo, remote_lid, rcluster_id, priv->term, 2, priv->sm_log.next_retrans_req_idx);
 	priv->sdev->fire = 1;
 
 	return;
