b27795417e66f99bf5237022ca9e283ae269d5fc
Vincent <Vincent.Riesop@gmail.com>
2020-01-21 09:35:11 +0100

    less verbose

--- core/asguard_payload_helper.c
+++ core/asguard_payload_helper.c
@@ -260,8 +260,8 @@ int setup_append_msg(struct consensus_priv *cur_priv, struct asguard_payload *sp
 		if(!retrans)
 			cur_priv->sm_log.next_index[target_id] += num_entries;
 
-		asguard_dbg("retrans=%d, target_id=%d, leader_last_idx=%d, next_idx=%d, prev_log_term=%d, num_entries=%d\n",
-					retrans, target_id, local_last_idx, next_index, prev_log_term, num_entries);
+		// asguard_dbg("retrans=%d, target_id=%d, leader_last_idx=%d, next_idx=%d, prev_log_term=%d, num_entries=%d\n",
+		// 			retrans, target_id, local_last_idx, next_index, prev_log_term, num_entries);
 	}
 
 	// reserve space in asguard heartbeat for consensus LEAD
