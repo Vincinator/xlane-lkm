af85c2f09f41b0787bd1f8cec60dfb1d09635d5b
Vincent <Vincent.Riesop@gmail.com>
2020-02-06 15:21:38 +0100

    dbg fix

--- core/asguard_core.c
+++ core/asguard_core.c
@@ -166,10 +166,9 @@ void _handle_sub_payloads(struct asguard_device *sdev, int remote_lid, int clust
 		cur_ins->ctrl_ops.post_payload(cur_ins, remote_lid, cluster_id, payload);
 	}
 
-	if(instances - 1 > 0) {
-		asguard_dbg("processing another msg of payload.. (%d) \n", instances);
-		asguard_dbg("bcnt=%u, cur_offset=%u, remaining_offset=%u", bcnt, cur_offset, bcnt - cur_offset);
-	}
+	asguard_dbg("processing another msg of payload.. (%d) \n", instances);
+	asguard_dbg("bcnt=%u, cur_offset=%u, remaining_offset=%u", bcnt, cur_offset, bcnt - cur_offset);
+
 	// handle next payload
 	_handle_sub_payloads(sdev, remote_lid, cluster_id, payload + cur_offset, instances - 1, bcnt - cur_offset);
 }
