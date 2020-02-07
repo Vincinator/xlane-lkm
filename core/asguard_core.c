9535dc88fede00fdd6dc5334f576e318197ac3f6
Vincent <Vincent.Riesop@gmail.com>
2020-02-07 15:27:30 +0100

    dbg fix

--- core/asguard_core.c
+++ core/asguard_core.c
@@ -335,7 +335,7 @@ int asguard_core_register_nic(int ifindex,  int asguard_id)
 	score->sdevices[asguard_id]->is_leader = 0;
 
 	score->sdevices[asguard_id]->asguard_leader_wq =
-		 alloc_workqueue("asguard_leader", WQ_UNBOUND | WQ_HIGHPRI, 0);
+		 alloc_workqueue("asguard_leader", WQ_HIGHPRI, 0);
 
 	for (i = 0; i < MAX_PROTO_INSTANCES; i++)
 		score->sdevices[asguard_id]->instance_id_mapping[i] = -1;
@@ -610,7 +610,7 @@ static int __init asguard_connection_core_init(void)
 	//init_asguard_proto_info_interfaces();
 
 	/* Allocate Workqueues */
-	asguard_wq = alloc_workqueue("asguard", WQ_UNBOUND | WQ_HIGHPRI, 0);
+	asguard_wq = alloc_workqueue("asguard", WQ_HIGHPRI, 0);
 
 
 	return 0;
