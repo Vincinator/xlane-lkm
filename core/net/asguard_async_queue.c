d7f4b21d42189fc940f45177bad43fb56c47965b
Vincent Riesop <Vincent.Riesop@sap.com>
2020-02-13 11:05:39 +0100

    Fixes

--- core/net/asguard_async_queue.c
+++ core/net/asguard_async_queue.c
@@ -15,6 +15,7 @@ int init_asguard_async_list_of_queues(struct asguard_async_head_of_queues_priv *
     }
 
     INIT_LIST_HEAD(&((*aapriv)->head_of_aa_queues));
+    asguard_dbg("Async Queues List initialized\n");
 
     return 0;
 }
@@ -32,7 +33,8 @@ int init_asguard_async_queue(struct asguard_async_head_of_queues_priv *aapriv, s
     INIT_LIST_HEAD(&(*new_queue)->head_of_async_pkt_queue);
 
     list_add_tail(&(*new_queue)->head_of_async_pkt_queue, &aapriv->head_of_aa_queues);
-    
+    asguard_dbg("Async Queue initialized\n");
+
     return 0;
 
 }
@@ -41,7 +43,9 @@ EXPORT_SYMBOL(init_asguard_async_queue);
 int enqueue_async_pkt(struct asguard_async_queue_priv *aqueue, struct asguard_async_pkt *apkt)
 {   
 
+
     list_add_tail(&apkt->async_pkts_head, &aqueue->head_of_async_pkt_queue);
+    asguard_dbg("Packet enqueued\n");
 
     return 0;
 }
@@ -52,6 +56,7 @@ int push_front_async_pkt(struct asguard_async_queue_priv *aqueue, struct asguard
 {
 
     list_add(&apkt->async_pkts_head, &aqueue->head_of_async_pkt_queue);
+    asguard_dbg("Packet pushed to front of queue\n");
 
     return 0;
 
@@ -68,7 +73,9 @@ EXPORT_SYMBOL(push_front_async_pkt);
     if(queued_apkt != NULL)
         list_del(&queued_apkt->async_pkts_head);
 
-    return queued_apkt;
+     asguard_dbg("Packet dequeued\n");
+
+     return queued_apkt;
 }
 EXPORT_SYMBOL(dequeue_async_pkt);
 
@@ -80,6 +87,7 @@ struct asguard_async_pkt *create_async_pkt(struct net_device *ndev, u32 dst_ip, 
     apkt = kmalloc(sizeof(struct asguard_async_pkt), GFP_KERNEL);
 
     apkt->skb = reserve_skb(ndev, dst_ip, dst_mac, &apkt->payload_ptr);
+    asguard_dbg("Packet created\n");
 
     return apkt;
 }
@@ -88,5 +96,7 @@ EXPORT_SYMBOL(create_async_pkt);
 void ring_aa_doorbell(struct asguard_async_queue_priv *aqueue) 
 {
     aqueue->doorbell++;
+    asguard_dbg("Doorbell rung\n");
+
 }
 EXPORT_SYMBOL(ring_aa_doorbell);
