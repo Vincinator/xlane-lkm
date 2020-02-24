486305af8231d2ea8363bbdadfcee5cd7261d19f
Vincent Riesop <Vincent.Riesop@sap.com>
2020-02-24 17:41:27 +0100

    less verbose

--- core/asguard_core.c
+++ core/asguard_core.c
@@ -233,18 +233,20 @@ void asguard_post_payload(int asguard_id, void *payload, u16 headroom, u32 cqe_b
 	struct asguard_pkt_work_data *work;
 	uint64_t ts2, ts3;
 	char *remote_mac = ((char *) payload) + headroom + 6;
-	char *user_data = ((char *) payload) + headroom + 43;
+	char *user_data = ((char *) payload) + headroom + 42;
 
-    /* */
+    /*
     asguard_dbg("cqe_bcnt=%u, SRC MAC=%pM", cqe_bcnt, remote_mac);
-
+ */
 	ts2 = RDTSC_ASGUARD;
 
+/*
    print_hex_dump(KERN_DEBUG, "pkt (no headroom): ", DUMP_PREFIX_NONE, 32, 1,
                    payload + headroom, 64 , 0);
 
     print_hex_dump(KERN_DEBUG, "user data: ", DUMP_PREFIX_NONE, 32, 1,
                    user_data, 16 , 0);
+*/
 
 
 
