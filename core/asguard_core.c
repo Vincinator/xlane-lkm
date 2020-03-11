7b84b6049ad38772e2414a400aa5d2fba13bcdab
Vincent Riesop <Vincent.Riesop@gmail.com>
2020-03-11 10:09:09 +0100

    lw: 2, fw: 2

--- core/asguard_core.c
+++ core/asguard_core.c
@@ -391,7 +391,7 @@ int asguard_core_register_nic(int ifindex,  int asguard_id)
 	score->sdevices[asguard_id]->is_leader = 0;
 
 	score->sdevices[asguard_id]->asguard_leader_wq =
-		 alloc_workqueue("asguard_leader", WQ_HIGHPRI | WQ_CPU_INTENSIVE | WQ_UNBOUND, 1);
+		 alloc_workqueue("asguard_leader", WQ_HIGHPRI | WQ_CPU_INTENSIVE | WQ_UNBOUND, 2);
 
 	for (i = 0; i < MAX_PROTO_INSTANCES; i++)
 		score->sdevices[asguard_id]->instance_id_mapping[i] = -1;
@@ -669,7 +669,6 @@ static int __init asguard_connection_core_init(void)
 	/* Allocate Workqueues */
 	asguard_wq = alloc_workqueue("asguard",  WQ_HIGHPRI | WQ_CPU_INTENSIVE | WQ_UNBOUND | WQ_MEM_RECLAIM | WQ_FREEZABLE, 2);
     asguard_wq_lock = 0;
-
 
 	return 0;
 error:
