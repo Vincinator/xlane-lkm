74987931977c380030079de1d5f8f86191ed4f7d
Vincent Riesop <Vincent.Riesop@gmail.com>
2020-04-02 13:42:53 +0200

    dbg

--- core/logic_layer/consensus/asguard_consensus.c
+++ core/logic_layer/consensus/asguard_consensus.c
@@ -167,12 +167,11 @@ void set_ae_data(unsigned char *pkt,
 			asguard_dbg("BUG! - entries cmd at %d is null", i);
 			return;
 		}
+
         print_hex_dump(KERN_DEBUG, "in log: ", DUMP_PREFIX_OFFSET, 64, 1,
                        entries[i]->cmd->data, sizeof(struct data_chunk), 0);
-        memcpy(cur_ptr, entries[i]->cmd->data, sizeof(struct data_chunk));
 
-        print_hex_dump(KERN_DEBUG, "in pkt: ", DUMP_PREFIX_OFFSET, 64, 1,
-                       cur_ptr, sizeof(struct data_chunk), 0);
+        memcpy(cur_ptr, entries[i]->cmd->data, sizeof(struct data_chunk));
 
 		// TODO: u32bit ptr increased twice to land at next data_chunk..
 		//         ... however, we need a stable method if we change the data_chunk size!!
