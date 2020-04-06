a66bbab3d47244bf18626d28371c9bef7282f9b1
Vincent Riesop <Vincent.Riesop@gmail.com>
2020-04-06 17:11:22 +0200

    dbg

--- core/uface/ringbuffer.c
+++ core/uface/ringbuffer.c
@@ -46,6 +46,8 @@ int append_rb(struct asg_ring_buf *buf, struct data_chunk *data) {
     print_hex_dump(KERN_DEBUG, "rx consensus hexdump: ", DUMP_PREFIX_NONE, 16,1,
                    &buf->ring[buf->write_idx], sizeof(struct data_chunk), 0);
 
+    asguard_dbg("write_idx: %d, read_idx: %d, turn: %d \n", buf->write_idx, buf->read_idx, buf->turn);
+
     buf->write_idx++;
 
     /* index starts at 0! */
