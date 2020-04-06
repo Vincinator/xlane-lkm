94921f6225e156750ac11174745b3e0ef0921ffc
Vincent Riesop <Vincent.Riesop@gmail.com>
2020-04-06 19:18:00 +0200

    fix

--- core/uface/ringbuffer.c
+++ core/uface/ringbuffer.c
@@ -106,14 +106,14 @@ int check_entry(struct asg_ring_buf *buf) {
     }
 
     /* Check if read_idx points to a header */
-    if(validate_header_key((char*) &buf->ring[buf->read_idx]) == 0 ){
+    if(validate_header_key((char*) &buf->ring[buf->read_idx].data) == 0 ){
         asguard_error("first item is not a header! Read Idx = %d, write IDX = %d", buf->read_idx, buf->write_idx);
         print_hex_dump(KERN_DEBUG, "This should be the header: ", DUMP_PREFIX_OFFSET, 64, 1,
-                       &buf->ring[buf->read_idx], sizeof(struct data_chunk), 0);
+                       &buf->ring[buf->read_idx].data, sizeof(struct data_chunk), 0);
         return 1;
     }
 
-    num_of_chunks = get_num_of_chunks((char*) &buf->ring[buf->read_idx]);
+    num_of_chunks = get_num_of_chunks((char*) &buf->ring[buf->read_idx].data);
 
     /* Check if enough Chunks are in the buffer */
     if(buf->turn) {
