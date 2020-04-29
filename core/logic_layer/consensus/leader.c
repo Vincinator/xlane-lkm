413e0287a36ccd982fc8c2b8b661f8f5ad99d9a6
Vincent Riesop <Vincent.Riesop@gmail.com>
2020-04-29 15:42:50 +0200

    more hex dbg

--- core/logic_layer/consensus/leader.c
+++ core/logic_layer/consensus/leader.c
@@ -241,7 +241,7 @@ int leader_process_pkt(struct proto_instance *ins, int remote_lid, int rcluster_
 			if(priv->sm_log.last_idx < param4) {
 				asguard_error("(1) match index (%d) is greater than local last idx (%d)!\n", param4, priv->sm_log.last_idx);
                 print_hex_dump(KERN_DEBUG, "response hexdump: ", DUMP_PREFIX_NONE, 16,1,
-                               pkt, 16, 0);
+                               pkt, 32, 0);
 
                 break; // ignore this faulty packet
 			}
