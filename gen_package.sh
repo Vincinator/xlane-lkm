38796e3279938694ec6ca32c1c9cebb31688a788
Vincent Riesop <v.riesop@sap.com>
2021-01-27 07:36:57 -0800

    added package generator script

--- gen_package.sh
+++ gen_package.sh
@@ -8,8 +8,13 @@ FOUND_KERNEL_MODULE=false
 FOUND_DPDK_APP=false
 FOUND_USERSPACE_APP=false
 
+ASGARD_KERNEL_MODULE_PKG_NAME=asgard-lkm
+ASGARD_DPDK_APP_PKG_NAME=asgard-dpdk
+ASGARD_USERSPACE_PKG_NAME=asgard-app
 
-mkdir -p packages
+PACKAGE_FOLDER=packages
+
+mkdir -p ${PACKAGE_FOLDER}
 
 echo "Checking if binaries are present:"
 if [[ -f "$ASGARD_LKM_REL_PATH" ]]; then
@@ -27,16 +32,21 @@ if [[ -f "$ASGARD_USERSPACE_REL_PATH" ]]; then
     echo " ... Found $ASGARD_USERSPACE_REL_PATH"
 fi
 
-
 echo "Packaging archives for found binaries:"
 if [ "$FOUND_KERNEL_MODULE" = true ] ; then
-  echo ""
+  tar -czf "$PACKAGE_FOLDER/$ASGARD_KERNEL_MODULE_PKG_NAME.tar.gz" $ASGARD_LKM_REL_PATH
+  echo " ... created $PACKAGE_FOLDER/$ASGARD_KERNEL_MODULE_PKG_NAME.tar.gz"
+
 fi
 
 if [ "$FOUND_DPDK_APP" = true ] ; then
-  echo ""
+  tar -czf "$PACKAGE_FOLDER/$ASGARD_DPDK_APP_PKG_NAME.tar.gz" $ASGARD_DPDK_REL_PATH
+  echo " ... created $PACKAGE_FOLDER/$ASGARD_DPDK_APP_PKG_NAME.tar.gz"
+
 fi
 
 if [ "$FOUND_USERSPACE_APP" = true ] ; then
-    echo ""
+  tar -czf "$PACKAGE_FOLDER/$ASGARD_USERSPACE_PKG_NAME.tar.gz" $ASGARD_USERSPACE_REL_PATH
+  echo " ... created $PACKAGE_FOLDER/$ASGARD_USERSPACE_PKG_NAME.tar.gz"
+
 fi
