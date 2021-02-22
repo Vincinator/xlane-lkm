391063f94743718af8f79621f7994e970b430b20
Vincent Riesop <v.riesop@sap.com>
2021-02-22 07:34:00 -0800

    fix

--- src/core/proto.c
+++ src/core/proto.c
@@ -14,18 +14,17 @@
 int register_protocol_instance(struct asgard_device *sdev, int instance_id, int protocol_id, int verbosity)
 {
 
-    int idx = sdev->num_of_proto_instances;
     int ret;
 
 
-    if (idx > MAX_PROTO_INSTANCES) {
+    if (instance_id > MAX_PROTO_INSTANCES) {
         ret = -EPERM;
         asgard_dbg("Too many instances exist, can not exceed maximum of %d instances\n", MAX_PROTOCOLS);
         asgard_dbg("Current active instances: %d\n", sdev->num_of_proto_instances);
 
         goto error;
     }
-    if(idx < 0) {
+    if(instance_id < 0) {
         ret = -EPERM;
         asgard_dbg("Invalid Instance ID: %d\n",instance_id);
         goto error;
@@ -37,21 +36,21 @@ int register_protocol_instance(struct asgard_device *sdev, int instance_id, int 
         goto error;
     }
 
-    sdev->protos[idx] = generate_protocol_instance(sdev, protocol_id);
+    sdev->protos[instance_id] = generate_protocol_instance(sdev, protocol_id);
 
-    if (!sdev->protos[idx]) {
+    if (!sdev->protos[instance_id]) {
         asgard_dbg("Could not allocate memory for new protocol instance!\n");
         ret = -ENOMEM;
         goto error;
     }
 
-    sdev->instance_id_mapping[instance_id] = idx;
+    sdev->instance_id_mapping[instance_id] = instance_id;
 
-    sdev->protos[idx]->instance_id = instance_id;
+    sdev->protos[instance_id]->instance_id = instance_id;
 
     sdev->num_of_proto_instances++;
 
-    sdev->protos[idx]->ctrl_ops.init(sdev->protos[idx], verbosity);
+    sdev->protos[instance_id]->ctrl_ops.init(sdev->protos[instance_id], verbosity);
 
     asgard_dbg("Registered  Protocol instance %d with protocol %s\n", instance_id, asgard_get_protocol_name(protocol_id));
 
