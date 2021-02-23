92f1a275d07290a350b77d0115a070541e4a7426
Vincent Riesop <v.riesop@sap.com>
2021-02-23 02:42:21 -0800

    fix

--- src/core/pkthandler.c
+++ src/core/pkthandler.c
@@ -119,6 +119,7 @@ void handle_sub_payloads(struct asgard_device *sdev, int remote_lid, int cluster
             asgard_dbg("No instance for protocol id %d were found. instances=%d", cur_proto_id, instances);
 
     } else {
+        asgard_dbg("posting Payload from %d to protocol instance %d of type %d\n", remote_lid, instances, cur_proto_id);
         cur_ins->ctrl_ops.post_payload(cur_ins, remote_lid, cluster_id, payload, ots);
     }
     // handle next payload
