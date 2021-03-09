f35ff0cb46dc2611bb6a0d288582a966e5beec76
Vincent Riesop <v.riesop@sap.com>
2021-03-09 15:50:07 +0100

    use setup.py instead of setup.cfg

--- asgard-cli/setup.py
+++ asgard-cli/setup.py
@@ -119,7 +119,7 @@ setup(
     # executes the function `main` from this package when invoked:
     entry_points={  # Optional
         'console_scripts': [
-            'acli=cli:main',
+            'acli=cli:entry_point',
         ],
     },
 
