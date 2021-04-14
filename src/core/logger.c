45bfd3b8d28940d85aceb6c59799871c5ec720a8
Vincent Riesop <v.riesop@sap.com>
2021-04-14 14:18:10 +0200

    try

--- src/core/logger.c
+++ src/core/logger.c
@@ -231,9 +231,9 @@ void dump_ingress_logs_to_file(struct asgard_device *sdev)
 void clear_ingress_logger(struct asgard_ingress_logger *ailog){
     int i;
 
-    if(ailog && ailog->per_node_logger) {
-        for(i = 0; i < CLUSTER_SIZE; i++)
-            clear_logger(&ailog->per_node_logger[i]);
+    if(ailog->per_node_logger) {
+        for(i = 0; i < ailog->num_of_nodes; i++)
+            AFREE(ailog->per_node_logger[i].events)
 
         AFREE(ailog->per_node_logger);
     }
